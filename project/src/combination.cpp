461e0cb01b99b164349d78b06ae2ec9931aca2a8
Philipp Rauser <philipp.rauser@tuhh.de>
2024-09-28 16:17:50 +0200

    Intersection WOrks now for three objects as well

--- project/src/combination.cpp
+++ project/src/combination.cpp
@@ -24,27 +24,65 @@ void Union::intersection(const Ray& ray, World* scene, intersectionInfo* closest
         }
     }
 }
-
+/**
 void Intersection::intersection(const Ray& ray, World* scene, intersectionInfo* closestHit){
     struct intersectionInfo intersectionHit;
-    intersectionHit = {.didHit = false, .t = INFINITY, .position = Vec4f(0.0), .normal = Vec4f(0.0), .dir = Vec4f(0.0), .object = nullptr};
 
     // it only saves the intersection info in closestHit if it hits all objects of the intersection
     for(auto object : objects){
+        intersectionHit = {.didHit = false, .t = INFINITY, .position = Vec4f(0.0), .normal = Vec4f(0.0), .dir = Vec4f(0.0), .object = nullptr};
         object->intersection(ray, scene, &intersectionHit);
-        if(intersectionHit.didHit){
-            if(objects.size() == 1){
+
+        if(!intersectionHit.didHit) continue;
+
+        if(objects.size() == 1){
+            *closestHit = intersectionHit;
+            break;
+        }
+
+        for(auto other : objects) {
+
+            if(other == object ) continue;
+
+            if(other->isIncluded(intersectionHit.position)){
+                if(!closestHit->didHit || intersectionHit.t < closestHit->t) {
+                    *closestHit = intersectionHit;
+                }
+            }else {
+                intersectionHit = {.didHit = false, .t = INFINITY, .position = Vec4f(0.0), .normal = Vec4f(0.0), .dir = Vec4f(0.0), .object = nullptr};
                 *closestHit = intersectionHit;
                 break;
             }
-            for(auto other : objects) {
-                if(other == object ) continue;
-                if(!other->isIncluded(intersectionHit.position)){
-                    intersectionHit = {.didHit = false, .t = INFINITY, .position = Vec4f(0.0), .normal = Vec4f(0.0), .dir = Vec4f(0.0), .object = nullptr};
-                    break;
-                }else {
-                    *closestHit = intersectionHit;
-                }
+        }
+    }
+
+} */
+
+void Intersection::intersection(const Ray& ray, World* scene, intersectionInfo* closestHit){
+    std::vector<intersectionInfo> intersectionHits;
+    intersectionHits.reserve(objects.size());
+
+    // calculate all intersections
+    for(int i = 0; i < objects.size(); i++){
+        intersectionHits.push_back({.didHit = false, .t = INFINITY, .position = Vec4f(0.0), .normal = Vec4f(0.0), .dir = Vec4f(0.0), .object = nullptr});
+        objects[i]->intersection(ray, scene, &intersectionHits[i]);
+    }
+
+    for(auto intersectionHit : intersectionHits){
+        if(!intersectionHit.didHit) continue;
+
+        bool isInAll = true;
+        for(auto object : objects) {
+            if(object == intersectionHit.object) continue;
+            if(!object->isIncluded(intersectionHit.position)){
+                isInAll = false;
+                break;
+            }
+        }
+
+        if(isInAll) {
+            if(!closestHit->didHit || intersectionHit.t < closestHit->t) {
+                *closestHit = intersectionHit;
             }
         }
     }
